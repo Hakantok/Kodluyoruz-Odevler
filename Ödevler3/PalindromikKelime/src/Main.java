00184f2e05c2b7647788bfea542c0e9b8411b5d9
Hakantok <87623485+Hakantok@users.noreply.github.com>
2022-10-13 00:53:58 +0300

    Palindromik Kelime

---
+++ Ödevler3/PalindromikKelime/src/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+	
+	public static boolean isPalindrom(String str) {
+		
+		int i = 0;
+		int j = str.length() - 1;
+		while(i < j) {
+			if(str.charAt(i) != str.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		
+		return true;
+	}
+	
+	public static boolean isPalindrom2(String str) {
+		
+		String reverse = "";
+		
+		for(int i = str.length() - 1; i <= 0; i--) {
+			reverse += str.charAt(i);
+			
+		}
+		
+		return str.equals(reverse);
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(isPalindrom("kayak"));
+
+	}
+
+}
