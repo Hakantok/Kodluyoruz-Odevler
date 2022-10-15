f998307b5bc7fb65c90c25f7024118b3d6030643
Hakantok <87623485+Hakantok@users.noreply.github.com>
2022-10-15 17:16:09 +0300

    SQL Ödevler

---
+++ SQL/Ödev5.sql
@@ -0,0 +1,18 @@
+-- 1.Senaryo
+SELECT * FROM film
+WHERE title LIKE '%n' 
+ORDER BY length DESC 
+LIMIT 5;
+
+-- 2.Senaryo
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+
+-- 3.Senaryo
+SELECT * FROM customer
+WHERE store_id = '1'
+ORDER BY last_name DESC
+LIMIT 4;
